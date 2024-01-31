3fe3d45bbbd270115f181b2e0e75ea584004f7c0
furkan-firat <furkanfirat@live.com>
2024-01-31 07:29:40 +0300

    add routes controllers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import ejs from "ejs";
 
+import pageRouter from "./routes/pageRouter.js";
+
 const app = express();
 
 // TEMPLATE ENGINE
@@ -10,17 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // ROUTING
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRouter);
 
 const port = 3000;
 app.listen(port, () => {
