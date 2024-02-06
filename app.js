08f938d49f703c3c36860e94575cc4192a7fefac
furkan-firat <furkanfirat@live.com>
2024-02-06 14:58:22 +0300

    add session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import ejs from "ejs";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import session from "express-session";
 
 import pageRoute from "./routes/pageRoute.js";
 import courseRoute from "./routes/courseRoute.js";
@@ -18,12 +19,26 @@ mongoose.connect("mongodb://127.0.0.1:27017/smartedu-db").then(() => {
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
+// GLOBAL VARIABLE
+global.userIN = null;
+
 // MIDDLEWARE
 app.use(express.static("public"));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: "my-session-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // ROUTING
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -21,18 +21,17 @@ export const loginUser = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-
     if (!user) {
       return res.status(400).send("Invalid email or password");
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
-
     if (!validPassword) {
       return res.status(400).send("Invalid email or password");
     }
 
-    res.send("Login successful");
+    req.session.userID = user._id;
+    res.redirect("/");
   } catch (error) {
     console.log(error);
     res.status(400).json({
@@ -40,4 +39,15 @@ export const loginUser = async (req, res) => {
       error,
     });
   }
+};
+
+export const logoutUser = async (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.redirect("/login");
+    }
+  });
 };
